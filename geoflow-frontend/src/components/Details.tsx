7f0e572a00d2addb81e0e6f0cb61280ee2bdcdfe
João Pedro de Araújo <joao.pedrocdearaujo@gmail.com>
2026-04-22 00:24:30 -0300

    feat: add Details component for record overview

---
+++ geoflow-frontend/src/components/Details.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Driver from './cards/Driver'
+import Location from './cards/Location'
+import Info from './cards/Info'
+
+type Props = {}
+
+export default function Details(props: Props) {
+  return (
+    <div className='flex flex-col items-center jusitfy-start w-full h-full gap-4 p-4'>
+        <Driver />
+        <div className='flex flex-col w-full gap-2'>
+          <p className="text-sm text-[#737982] font-semibold">Localização</p>
+          <Location />
+        </div>
+        <div className='flex flex-col w-full gap-2'>
+          <p className="text-sm text-[#737982] font-semibold">Informações da Entrega</p>
+          <Info />
+        </div>
+    </div>
+  )
+}
