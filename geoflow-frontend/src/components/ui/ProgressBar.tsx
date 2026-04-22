d211f08f4a17387e3a7c680fbe2ca23c04a560ba
João Pedro de Araújo <joao.pedrocdearaujo@gmail.com>
2026-04-22 00:25:46 -0300

    feat(ui): add ProgressBar component for status tracking

---
+++ geoflow-frontend/src/components/ui/ProgressBar.tsx
@@ -0,0 +1,18 @@
+import { Progress } from "./progress"
+
+
+type Props = {
+    value: number
+}
+
+export default function ProgressBar({value}: Props){
+  return (
+           <div className="flex flex-col gap-2">
+            <div className="flex justify-between">
+                <p className="text-xs text-[#737982] font-semibold">Progress</p>
+                <p className="text-xs text-[#737982] font-semibold">33%</p>
+            </div>
+            <Progress value={value}/>
+        </div>
+  )
+}
