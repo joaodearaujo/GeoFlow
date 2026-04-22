ad68496361e1a8f6045e2edf0f42112731e901c5
João Pedro de Araújo <joao.pedrocdearaujo@gmail.com>
2026-04-22 00:25:29 -0300

    feat: add Info card for general metadata display

---
+++ geoflow-frontend/src/components/cards/Info.tsx
@@ -0,0 +1,45 @@
+import { Box, Clock } from "lucide-react"
+import ProgressBar from "../ui/ProgressBar"
+
+type Props = {}
+
+export default function Info(props: Props) {
+  return (
+        <div className="flex justify-center hover:bg-[#292929] bg-[#252525] cursor-pointer flex-col w-full shrink-0 h-fit p-4 rounded-xl border border-white/10">
+            <div className="flex flex-col gap-2">
+            
+                <div className="flex flex-col gap-4">
+                    <div className="flex justify-between">
+                        <div className="flex items-center gap-2 w-full h-fit">
+                            <ClockIcon />
+                            <p className="text-xs text-[#737982] font-semibold">Tempo de espera</p>
+                        </div>
+                        <p className="text-xs font-semibold text-nowrap">25 min</p>
+                    </div>
+
+                </div>  
+                <div className="flex flex-col gap-4">
+                    <div className="flex justify-between">
+                        <div className="flex items-center gap-2 w-full h-fit">
+                            <BoxIcon />
+                            <p className="text-xs text-[#737982] font-semibold">Peso</p>
+                        </div>
+                        <p className="text-xs font-semibold text-nowrap">2.5 kg</p>
+                    </div>
+                </div>  
+
+                <hr />
+
+                <ProgressBar value={33}/>
+
+            </div>
+        </div>
+  )
+}
+
+function ClockIcon() {
+    return <Clock size={16} className="text-[#737982]"/>
+}
+function BoxIcon() {
+    return <Box size={16} className="text-[#737982]"/>
+}
