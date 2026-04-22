852aa5a55390ffa4460067de426bf22acb11cbe0
João Pedro de Araújo <joao.pedrocdearaujo@gmail.com>
2026-04-22 00:25:21 -0300

    feat: add Driver card component to display operator info

---
+++ geoflow-frontend/src/components/cards/Driver.tsx
@@ -0,0 +1,31 @@
+import { Phone, User } from "lucide-react"
+
+type Props = {}
+
+export default function Driver(props: Props) {
+  return (
+    <div className="flex justify-center hover:bg-[#292929] bg-[#252525] cursor-pointer flex-col w-full shrink-0 h-fit p-4  rounded-xl border border-white/10">
+        <div className="flex flex-col gap-4">
+            <div className="flex items-center gap-3 w-full h-fit">
+                <DriverIcon />
+                <div className="flex h-10 flex-col gap-1">
+                    <p className="text-xs text-[#737982] font-semibold">Motorista</p>
+                    <p className="text-md font-semibold leading-none">Carlos Silva</p>
+                </div>
+            </div>
+            <div className="flex items-center gap-2 w-full h-fit">
+                <PhoneIcon />
+                <p className="text-sm text-[#FFFFFF] font-regular">+55 11 98765-4321</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+function DriverIcon() {
+    return <div className="rounded-full bg-blue-500/30 size-10 items-center justify-center flex"><User className="text-blue-400"/></div>
+}
+
+function PhoneIcon() {
+    return <Phone size={20} className="text-[#737982]"/>
+}
